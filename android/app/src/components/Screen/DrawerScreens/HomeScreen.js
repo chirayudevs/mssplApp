7c13993d3f6c8f3404333ab20f429d6a44117437
Chirayu Modi <chirayum018@gmail.com>
2023-07-12 15:49:10 +0530

    feat: Added Drawer navigator screen and home screen

---
+++ android/app/src/components/Screen/DrawerScreens/HomeScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, padding: 16}}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+          <Text
+            style={{
+              fontSize: 20,
+              textAlign: 'center',
+              marginBottom: 16,
+            }}>
+            Home Screen
+          </Text>
+        </View>
+        <Text
+          style={{
+            fontSize: 18,
+            textAlign: 'center',
+            color: 'grey'
+          }}>
+          Splash, login and register
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            textAlign: 'center',
+            color: 'grey'
+          }}
+        >
+          www.msspl.com
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
+};
+
+export default HomeScreen;
